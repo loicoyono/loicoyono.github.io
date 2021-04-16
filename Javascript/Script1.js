f76fc6cf6013591f6c8b049461e8a840497efe47
loicoyono <73126674+loicoyono@users.noreply.github.com>
2021-04-16 13:11:46 +0100

    hberger le site à Zambo

---
+++ Javascript/Script1.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+//Debut barre de depart
+const Liens = document.getElementsByClassName('cond');
+Liens.addEventListener("click", function () {
+    alert(Bonjour);
+});
+
+
+//Fin Barre de Depart
--- README.md
+++
@@ -1,1 +0,0 @@
-# loicoyono.github.io
